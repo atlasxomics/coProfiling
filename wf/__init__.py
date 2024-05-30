c8c52e43fb6a2f6217e79ac6ac0152ea22a9bdd6
jpmcga <jpaulmcgann@gmail.com>
2024-05-30 17:41:15 -0400

    fix: correct LaunchPlan

--- wf/__init__.py
+++ wf/__init__.py
@@ -34,8 +34,8 @@ class Run:
 
 
 class Genome(Enum):
-    mm10 = 'mm10'
-    hg38 = 'hg38'
+    mm10 = "mm10"
+    hg38 = "hg38"
 
 
 @large_task(retries=0)
@@ -57,30 +57,30 @@ def coProf_task(
     os.makedirs(output_dir, exist_ok=True)
 
     # Turn on virtual dev for png in Rmd
-    _xvfb_cmd = ['Xvfb', ':99', '-screen', '0', '1024x768x16']
+    _xvfb_cmd = ["Xvfb", ":99", "-screen", "0", "1024x768x16"]
     subprocess.Popen(_xvfb_cmd)
-    os.environ['DISPLAY'] = ':99'
+    os.environ["DISPLAY"] = ":99"
 
     _report_cmd = [
         "Rscript",
         "/root/wf/coProf.R",
         project_name,
         genome.value,
-        f'{spot_size}',
-        f'{cluster_resolution}',
-        f'{tile_size}',
-        f'{min_TSS}',
-        f'{min_frags}',
-        f'{lsi_iterations}',
-        f'{lsi_resolution}',
-        f'{lsi_varfeatures}'
+        f"{spot_size}",
+        f"{cluster_resolution}",
+        f"{tile_size}",
+        f"{min_TSS}",
+        f"{min_frags}",
+        f"{lsi_iterations}",
+        f"{lsi_resolution}",
+        f"{lsi_varfeatures}"
     ]
     runs = [
         (
-            f'{run.run_id},'
-            f'{run.gex_dir.local_path},'
-            f'{run.fragments_file.local_path},'
-            f'{run.spatial_dir.local_path},'
+            f"{run.run_id},"
+            f"{run.gex_dir.local_path},"
+            f"{run.fragments_file.local_path},"
+            f"{run.spatial_dir.local_path},"
         )
         for run in runs
     ]
@@ -95,26 +95,26 @@ def coProf_task(
 
 
 metadata = LatchMetadata(
-    display_name='coProfiling_wf',
+    display_name="coProfiling_wf",
     author=LatchAuthor(
-        name='AtlasXomics, Inc.',
-        email='nouroddinc@atlasxomics.com',
-        github='github.com/atlasxomics/coProfiling',
+        name="AtlasXomics, Inc.",
+        email="nouroddinc@atlasxomics.com",
+        github="github.com/atlasxomics/coProfiling",
     ),
-    repository='https://github.com/atlasxomics/coProfiling',
-    license='MIT',
+    repository="https://github.com/atlasxomics/coProfiling",
+    license="MIT",
     parameters={
-        'runs': LatchParameter(
-            display_name='runs',
-            description='List of runs to be analyzed; each run must contain a \
+        "runs": LatchParameter(
+            display_name="runs",
+            description="List of runs to be analyzed; each run must contain a \
                         run_id and fragments.tsv.gz file; gene expression \
-                        and spatial folder for SpatialDimPlot.',
+                        and spatial folder for SpatialDimPlot.",
             batch_table_column=True,
             samplesheet=True
         ),
-        'project_name': LatchParameter(
-            display_name='project name',
-            description='Name of output directory in coProfiling_outputs/',
+        "project_name": LatchParameter(
+            display_name="project name",
+            description="Name of output directory in coProfiling_outputs/",
             batch_table_column=True,
             rules=[
                 LatchRule(
@@ -123,62 +123,62 @@ metadata = LatchMetadata(
                 )
             ]
         ),
-        'genome': LatchParameter(
-            display_name='genome',
-            description='Reference genome to be used for geneAnnotation',
+        "genome": LatchParameter(
+            display_name="genome",
+            description="Reference genome to be used for geneAnnotation",
             batch_table_column=True,
         ),
-        'spot_size': LatchParameter(
-            display_name='spot size',
-            description='The size of the spots in the spatial maps',
-            batch_table_column=True,
-            hidden=True
-        ),
-        'cluster_resolution': LatchParameter(
-            display_name='cluster resolution',
-            description='resolution parameter for FindClusters function',
+        "spot_size": LatchParameter(
+            display_name="spot size",
+            description="The size of the spots in the spatial maps",
             batch_table_column=True,
             hidden=True
         ),
-        'tile_size': LatchParameter(
-            display_name='tile size',
-            description='The size of the tiles used for binning counts in the \
-                        TileMatrix.',
+        "cluster_resolution": LatchParameter(
+            display_name="cluster resolution",
+            description="resolution parameter for FindClusters function",
             batch_table_column=True,
             hidden=True
         ),
-        'min_TSS': LatchParameter(
-            display_name='minimum TSS',
-            description='The minimum numeric transcription start site (TSS) \
+        "tile_size": LatchParameter(
+            display_name="tile size",
+            description="The size of the tiles used for binning counts in the \
+                        TileMatrix.",
+            batch_table_column=True,
+            hidden=True
+        ),
+        "min_TSS": LatchParameter(
+            display_name="minimum TSS",
+            description="The minimum numeric transcription start site (TSS) \
                         enrichment score required for a cell to pass \
-                        filtering.',
+                        filtering.",
             batch_table_column=True,
             hidden=True
         ),
-        'min_frags': LatchParameter(
-            display_name='minimum fragments',
-            description='The minimum number of mapped ATAC-seq fragments \
-                        required per cell to pass filtering.',
+        "min_frags": LatchParameter(
+            display_name="minimum fragments",
+            description="The minimum number of mapped ATAC-seq fragments \
+                        required per cell to pass filtering.",
             batch_table_column=True,
             hidden=True
         ),
-        'lsi_iterations': LatchParameter(
-            display_name='LSI iterations',
-            description='iterations parameter from addIterativeLSI function.',
+        "lsi_iterations": LatchParameter(
+            display_name="LSI iterations",
+            description="iterations parameter from addIterativeLSI function.",
             batch_table_column=True,
             hidden=True
         ),
-        'lsi_resolution': LatchParameter(
-            display_name='LSI resolution',
-            description='resolution parameter from \
-                        addIterativeLSI/clusterParams function.',
+        "lsi_resolution": LatchParameter(
+            display_name="LSI resolution",
+            description="resolution parameter from \
+                        addIterativeLSI/clusterParams function.",
             batch_table_column=True
         ),
-        'lsi_varfeatures': LatchParameter(
-            display_name='LSI varFeatures',
-            description='varFeatures parameter from addIterativeLSI function; \
+        "lsi_varfeatures": LatchParameter(
+            display_name="LSI varFeatures",
+            description="varFeatures parameter from addIterativeLSI function; \
                         each will correspond to a umap.pdf, the last in the \
-                        will be used to make the RDS object.',
+                        will be used to make the RDS object.",
             batch_table_column=True
         )},
 )
@@ -217,26 +217,26 @@ def coProfiling_workflow(
 
 LaunchPlan(
     coProfiling_workflow,
-    'defaults',
+    "defaults",
     {
-        'runs': [
+        "runs": [
             Run(
-                'default',
-                LatchDir('latch:///star_outputs/demo/STAR_outputsGene/raw'),
-                LatchFile('latch:///chromap_outputs/demo/chromap_output/fragments.tsv.gz'),
-                LatchDir('latch:///spatials/demo/spatial/')
-                )
+                "default",
+                LatchDir("latch:///star_outputs/demo/STAR_outputsGene/raw"),
+                LatchFile("latch://13502.account/sample_fqs/test/fragments.tsv.gz"),
+                LatchDir("latch://atx-illumina.mount/Images_spatial/D1372/spatial")
+            )
         ],
-        'project_name': 'demo',
-        'genome': Genome.hg38,
-        'spot_size': 1,
-        'cluster_resolution': 0.5,
-        'tile_size': 5000,
-        'min_TSS': 2.0,
-        'min_frags': 0,
-        'lsi_iterations': 2,
-        'lsi_resolution': 0.5,
-        'lsi_varfeatures': 25000
+        "project_name": "demo",
+        "genome": Genome.mm10,
+        "spot_size": 1,
+        "cluster_resolution": 0.5,
+        "tile_size": 5000,
+        "min_TSS": 2.0,
+        "min_frags": 0,
+        "lsi_iterations": 2,
+        "lsi_resolution": 0.5,
+        "lsi_varfeatures": 25000
     },
 )
 
